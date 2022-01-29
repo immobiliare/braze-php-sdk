7506ad782ef67f45c2a2c5633b97cdc11856bd44
antonioturdo <antonio.turdo@gmail.com>
2022-01-29 12:38:48 +0100

    feat: added tests for provided adapters

---
+++ tests/ClientAdapter/Psr18AdapterTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace ImmobiliareLabs\BrazeSDK\Test\ClientAdapter;
+
+use ImmobiliareLabs\BrazeSDK\ClientAdapter\Psr18Adapter;
+use ImmobiliareLabs\BrazeSDK\Exception\NotValidBaseURIException;
+use ImmobiliareLabs\BrazeSDK\Exception\TransportException;
+use ImmobiliareLabs\BrazeSDK\Region;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+class Psr18AdapterTest extends TestCase
+{
+    /** @var ClientInterface|MockObject */
+    private $httpClientMock;
+
+    /** @var UriFactoryInterface|MockObject */
+    private $uriFactoryMock;
+
+    /** @var RequestFactoryInterface|MockObject */
+    private $requestFactoryMock;
+
+    /** @var Psr18Adapter|MockObject */
+    private $psr18Adapter;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->httpClientMock = $this->createMock(ClientInterface::class);
+        $this->uriFactoryMock = $this->createMock(UriFactoryInterface::class);
+        $this->requestFactoryMock = $this->createMock(RequestFactoryInterface::class);
+        $streamFactoryMock = $this->createMock(StreamFactoryInterface::class);
+
+        $this->psr18Adapter = new Psr18Adapter($this->httpClientMock, $this->uriFactoryMock, $this->requestFactoryMock, $streamFactoryMock);
+    }
+
+    public function testSetBaseUri(): void
+    {
+        $uriMock = $this->createMock(UriInterface::class);
+
+        $this->uriFactoryMock->expects(self::once())
+            ->method('createUri')
+            ->willReturn($uriMock);
+
+        $uriMock->expects(self::once())
+            ->method('withScheme')
+            ->willReturn($uriMock);
+
+        $uriMock->expects(self::once())
+            ->method('withHost')
+            ->willReturn($uriMock);
+
+        $this->psr18Adapter->setBaseURI(Region::EU01);
+    }
+
+    public function testSetBaseUriWithException(): void
+    {
+        $this->expectException(NotValidBaseURIException::class);
+        
+        $this->psr18Adapter->setBaseURI('not_an_url');
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testSetHeaders(): void
+    {
+        $this->psr18Adapter->setHeaders(['Content-Type' => 'application/json']);
+    }
+
+    public function testMakeRequest(): void
+    {
+        $request = $this->createMock(RequestInterface::class);
+
+        $this->testSetBaseUri();
+        $this->testSetHeaders();
+
+        $this->httpClientMock->expects($this->once())
+            ->method('sendRequest');
+
+        $this->requestFactoryMock->expects($this->once())
+            ->method('createRequest')
+            ->willReturn($request);
+
+        $request->expects($this->once())
+            ->method('withHeader')
+            ->willReturn($request);
+
+        $request->expects($this->once())
+            ->method('withBody')
+            ->willReturn($request);
+
+        $this->psr18Adapter->makeRequest('GET', '/path', '{}');
+    }
+
+    public function testMakeRequestWithException(): void
+    {
+        $this->expectException(TransportException::class);
+
+        $clientException = $this->createMock(ClientExceptionInterface::class);
+
+        $this->testSetBaseUri();
+
+        $this->httpClientMock->expects($this->once())
+            ->method('sendRequest')
+            ->willThrowException($clientException);
+
+        $this->psr18Adapter->makeRequest('GET', '/path');
+    }
+
+    public function testResolveResponse(): void
+    {
+        $httpResponseMock = $this->createMock(ResponseInterface::class);
+
+        $httpResponseMock->expects($this->once())
+            ->method('getStatusCode')
+            ->willReturn(200);
+
+        $httpResponseMock->expects($this->once())
+            ->method('getBody')
+            ->willReturn('');
+
+        $resolvedResponse = $this->psr18Adapter->resolveResponse($httpResponseMock);
+
+        self::assertSame(200, $resolvedResponse->getStatusCode());
+    }
+}
