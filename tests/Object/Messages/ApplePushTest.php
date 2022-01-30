fd02b8d0f5f0f6941a6f27be03c02b6aa39a4745
antonioturdo <antonio.turdo@gmail.com>
2022-01-30 13:45:17 +0100

    feat: added tests for jsonSerialize methods

--- tests/Object/Messages/ApplePushTest.php
+++ tests/Object/Messages/ApplePushTest.php
@@ -16,10 +16,19 @@ class ApplePushTest extends TestCase
         $applePush->validate(true);
     }
 
+    public function testJsonSerialize(): void
+    {
+        $applePush = new ApplePush();
+        $applePush->content_available = true;
+
+        $serialized = $applePush->jsonSerialize();
+
+        $this->assertArrayHasKey('content-available', $serialized);
+    }
+
     public function validProvider(): array
     {
         $applePush1 = new ApplePush();
-
 
         return [
             [$applePush1]
