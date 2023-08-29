4827855935ab4d0d9c463c198d7aa126a2b11a4c
antonioturdo <antonio.turdo@gmail.com>
2023-08-29 14:40:16 +0200

    fix: add properties to Subscription/GetStatusResponse class

--- src/Response/Subscription/GetStatusResponse.php
+++ src/Response/Subscription/GetStatusResponse.php
@@ -6,4 +6,12 @@ use ImmobiliareLabs\BrazeSDK\Response\BaseResponse;
 
 class GetStatusResponse extends BaseResponse
 {
+    public ?array $status = null;
+
+    public function fillFromArray(array $params, bool $allowExtraProperties = false): void
+    {
+        parent::fillFromArray($params, $allowExtraProperties);
+
+        $this->status = $params['status'] ?? [];
+    }
 }
